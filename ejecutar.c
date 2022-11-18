e86b31994319cb92557d699fe6bcc798fac84bcd
EstebanGomez1 <79277954+EstebanGomez1@users.noreply.github.com>
2022-11-18 21:24:37 +0100

    correccion de ejecutar.c
    Faltaba la linea de codigo para crear la minishell hija.

--- ejecutar.c
+++ ejecutar.c
@@ -12,17 +12,19 @@
 pid_t ejecutar_orden(const char *orden, int *pbackgr)
 {
    char **args;
+   //char **aux = 0;
    pid_t pid = 0;
    int indice_ent = -1, indice_sal = -1; /* por defecto, no hay < ni > */
-  
    if ( (args=parser_orden(orden, &indice_ent, &indice_sal, pbackgr)) == NULL)
    {
    	if (pid != 0){
 	printf ("vacio");
+	}else{
+	pid = fork(); //creamos minishell hija
 	}
       	return(-1);
    }else if( (pid == 0) && (execvp(args[0], args) == -1)){
-	printf ("No encontrada");
+   	printf("No encontrada\n");
 	return pid;
    }else{
 	printf ("ejecutar2");
@@ -37,27 +39,19 @@ pid_t ejecutar_orden(const char *orden, int *pbackgr)
 
 void ejecutar_linea_ordenes(const char *orden)
 {
-	/*
-   pid_t pid;
+	
+   //pid_t pid;
    int backgr;
 	
-	if (strchr("&", const char *orden) != NULL){ //padre espera al hijo //no creo que vaya
-		
-	}
-	else {
-		
-	}
+
 	
-   Si la orden es compuesta, podra incluir aqui, en otra fase de la 
-   practica, el codigo para su tratamiento                          
- 
+   /* Si la orden es compuesta, podra incluir aqui, en otra fase de la 
+   practica, el codigo para su tratamiento  */                        
+ 	char *arg2[3] = {"sleep","10","&"};
+	execvp(arg2[0], arg2);
 
-   pid = ejecutar_orden(orden, &backgr);
-   */
-   printf("ejecutar linea de ordenes entrado");
-      
-       
-
+   ejecutar_orden(orden, &backgr);
+   
 
 
 }   
