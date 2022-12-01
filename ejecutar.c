932d1693a595a1a8eaa6cc4e21e9481a8c640277
EstebanGomez1 <79277954+EstebanGomez1@users.noreply.github.com>
2022-12-01 12:29:58 +0100

    documentacion de los archivos adjuntos

--- ejecutar.c
+++ ejecutar.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 #include "parser.h"
 #include "ejecutar.h"
 #include "libmemoria.h"
@@ -14,14 +13,15 @@ int ** crear_pipes ( int nordenes ) 
  int ** pipes = NULL ; 
  int i ;  
  pipes = ( int **) malloc ( sizeof ( int *) * ( nordenes - 1)); 
- for ( i = 0; i < ( nordenes - 1); i ++) { 
+ for ( i = 0; i < ( nordenes - 1); i ++) 
+	{ 
 	pipes [ i ] = ( int *) malloc ( sizeof ( int ) * 2); 
 	if (i=0){
 		if (nordenes>1) {
 		salida = pipes[0][1];
 		}else{
 		salida=STDOUT_FILENO;
-pid[0]=ejecutar_orden(ordenes[0],STDIN_FILENO,salida,&backgr);}}
+	pid[0]=ejecutar_orden(ordenes[0],STDIN_FILENO,salida,&backgr);}}
 	if ((i=nordenes-1)&&(nordenes>1)){
 	entrada=pipes[nordenes-2][0];
 	salida= STDOUT_FILENO;
@@ -32,7 +32,8 @@ pid[0]=ejecutar_orden(ordenes[0],STDIN_FILENO,salida,&backgr);}}
 	if(backgr=0&&pid[nordenes-1]>0){
 	waitpid();
 	free(pid);
-	free_ordenes_pipes(ordenes,pipes,nordenes);}
+	free_ordenes_pipes(ordenes,pipes,nordenes);
+	}
 return ( pipes );}
 
 pid_t ejecutar_orden(const char *orden, int *pbackgr)
@@ -44,33 +45,38 @@ pid_t ejecutar_orden(const char *orden, int *pbackgr)
 
    int indice_ent = -1, indice_sal = -1,entrada = 0, salida = 1; 		/* por defecto, no hay < ni > */
    if ( (args=parser_orden(orden, &indice_ent, &indice_sal, pbackgr)) == NULL)	//parser 
-   {
+   {	/*en el caso de que no haya argumentos por consola*/
 	//printf ("vacio\n");
       	return -1;
    }
-   if(indice_ent != -1){
+   if(indice_ent != -1)
+	{	/*en el caso de que haya una redireccion de entrada hacemos una llamada a la funcion que se encarga de la instruccion*/
 	redirec_entrada(args, indice_ent, &entrada);
 	}
-   if(indice_sal != -1){
+   if(indice_sal != -1)
+	{	/*en el caso de que haya una redireccion de salida hacemos una llamada a la funcion que se encarga de la isntruccion*/
 	redirec_salida(args, indice_sal, &salida);
 	}
 
-   pid = fork();
+   pid = fork(); /*creamos el proceso hijo mediante fork()*/
 
    if(pid == 0)
-	{ 
-	if(entrada != 0){
+   { 
+	if(entrada != 0)
+	{	/*hay redireccionamiento de entrada*/
 		dup2(entrada,0);
 	}
-	if(salida != 1){
+	if(salida != 1)
+	{	/*hay redireccionamiento de salida*/
 		dup2(salida,1);
 	}
 
-   	if(execvp(args[0], args) < 0){
+   	if(execvp(args[0], args) < 0)
+	{	/*ejecucion de la orden correspondiente*/
    		printf("Error execvp\n");
 		exit(-1);
-   		}
    	}
+   }
    entrada = 0;
    salida = 1;
    free_argumentos(args);
@@ -85,7 +91,6 @@ void ejecutar_linea_ordenes(const char *orden)
    char *string;
    char *instruccion;
    int **pipes;
-   
    instruccion=parser_pipes(orden,&nordenes);
    pipes=crear_pipes(nordenes);
 
--- redirecciones.c
+++ redirecciones.c
@@ -10,17 +10,16 @@
 /* funcion que abre el archivo situado en la posicion indice_entrada+1 */
 /* de la orden args y elimina de ella la redireccion completa          */
 
-
-
 void redirec_entrada(char **args, int indice_entrada, int *entrada)
 	{
 	int file;
 	file = open(args[indice_entrada+1], O_RDONLY);
-	if (file == -1 ){
+	if (file == -1 ){ /*en caso de error al abrir el archivo*/
+		
 		printf("Error redirec entrada\n");
 		
 	}
-	else{
+	else{ /*los argumentos '<' los ponemos a 0 al igual que el archivo para evitar errores*/
 		*entrada = file;
 		args[indice_entrada] = '\0';
 		args[indice_entrada+1] = '\0';
@@ -34,11 +33,11 @@ void redirec_salida(char **args, int indice_salida, int *salida)
 	{
 	int file2;
 	file2 = open(args[indice_salida+1],O_WRONLY | O_CREAT | O_TRUNC , 0600);
-	if (file2 == -1 ){
+	if (file2 == -1 ){  /*en caso de error al abrir el archivo*/
 		printf("Error redirec salida\n");
 		
 	}
-	else{
+	else{  /*los argumentos '<' los ponemos a 0 al igual que el archivo para evitar errores*/
 		*salida = file2;
 		args[indice_salida] = '\0';
 		args[indice_salida+1] = '\0';
